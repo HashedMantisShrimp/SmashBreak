591ce028af3aac5f10e6d55e5b5cdde87324576f
PauloB04 <54282145+PauloB04@users.noreply.github.com>
2019-08-20 08:21:58 +0300

    Optimized script

--- BallForce.cs
+++ BallForce.cs
@@ -1,31 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallForce : MonoBehaviour {
 
-	public Ball ball;
-
-	void Start () {
-		
-	}
-	
-
-	void Update () {
-		
-	}
+	public Ball ball; //Ball script is attached
 
 	public void ballForce(){
 		ball.BF = 25;
 		ball.counter_switch = true;
-
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
 		ballForce ();
 		Destroy(gameObject);
-
-
 	}
 }
