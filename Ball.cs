95c1352ef422d54ff9fba69718684ec3f865f55e
PauloB04 <54282145+PauloB04@users.noreply.github.com>
2019-08-20 08:53:45 +0300

    Optimized Script

--- Ball.cs
+++ Ball.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// z = -15.8
 public class Ball : MonoBehaviour
 {
 	public GameObject im;
@@ -12,14 +11,12 @@ public class Ball : MonoBehaviour
 	public int counter=0;
 	public bool counter_switch = false;
 	public float BF=12; //ball Force
-	bool mloc;
-	public PointsScript sm; // script manager for points
-	private int Sp=0; //Startting points when ballforce is turned on
+	public PointsScript sm; // PointsScript is attached
+	private int Sp=0; //Starting points when ballforce is turned on
 	private int Ap; //Actual points
 	public Material material1;
 	public Material material2;
-	Vector3 pos = Vector3.zero, pos1 = Vector3.zero, ms = Vector3.zero, msL = Vector3.zero; //camL = Vector3.zero;
-
+	Vector3 pos = Vector3.zero, pos1 = Vector3.zero, ms = Vector3.zero, msL = Vector3.zero; 
 	Vector2 ScreenCenter = new Vector2(Screen.width/2, Screen.height/2), posL = Vector2.zero, pos1L = Vector2.zero;
 
     
@@ -27,22 +24,16 @@ public class Ball : MonoBehaviour
     {
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		temp.GetComponent<MeshRenderer> ().material = material2;
-
     }
-
-
     
     void Update()
     {
-		//camL = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
 		ms = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y,2));
 		msL = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
 		float translation = Time.deltaTime;
-		//Debug.Log(Input.touchCount);
 
 		if (Input.touchCount == 1) 
 		{
-			//touch = Input.GetTouch (0);
 			pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch (0).position.x, 
 				Input.GetTouch (0).position.y, 1));
 			posL = new Vector2 (Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
@@ -50,7 +41,6 @@ public class Ball : MonoBehaviour
 		}
 		if (Input.touchCount == 2) 
 		{
-			//touch = Input.GetTouch (0);
 			pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch (0).position.x, 
 				Input.GetTouch (0).position.y, 1));
 			posL = new Vector2 (Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
@@ -58,27 +48,16 @@ public class Ball : MonoBehaviour
 				              Input.GetTouch (1).position.y, 1));
 			pos1L = new Vector2 (Input.GetTouch(1).position.x, Input.GetTouch(1).position.y);
 		}
-			
-
-
+		
       if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.MacOSX)
         {
-			//Vector3 explosionPos = transform.TransformDirection(new Vector3((msL.x-ScreenCenter.x)/(Screen.width/2)*10,(msL.y-ScreenCenter.y)/(Screen.height/2)*7,12));
 
 			if (Input.GetButtonDown("Fire1"))
        		 {
 
             GameObject clone;
 				clone = Instantiate(temp, ms, transform.rotation) as GameObject;
-				//clone.AddComponent<Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);>
-				//clone = Instantiate(temp, camL, transform.rotation) as GameObject;
-			/*	if(msL.x >= Screen.width/2 && msL.y <= Screen.height/2){ clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0.9f,-0.5f,1.5f) * 5.5f); }// right down forward
-				else if (msL.x >= Screen.width/2 && msL.y >= Screen.height/2) {clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0.9f,1f,1.5f) * 5.5f);} // right up forward
-				else if (msL.x <= Screen.width/2 && msL.y >= Screen.height/2) {clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(-0.9f,1f,1.5f) * 5.5f);} // left up forward	 	
-				else if (msL.x <= Screen.width/2 && msL.y <= Screen.height/2) {clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(-0.9f,-0.5f,1.5f) * 5.5f);} // left down forward
-				else if ((msL.x >= Screen.width / 2 && msL.y <= Screen.height / 2) == false && (msL.x >= Screen.width / 2 && msL.y >= Screen.height / 2) == false && (msL.x <= Screen.width / 2 && msL.y >= Screen.height / 2) == false && (msL.x <= Screen.width / 2 && msL.y <= Screen.height / 2) == false) {clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0,0,1.5f) * 5.5f);} // forward
-*/
-				//clone.GetComponent<Rigidbody>().velocity = (new Vector3(camL.x, camL.y, 10) - new Vector3(ms.x, ms.y, 0)*3); //J
+				
 				clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3((msL.x-ScreenCenter.x)/
 					(Screen.width/2)*10,(msL.y-ScreenCenter.y)/(Screen.height/2)*7,BF));	
 			
@@ -112,7 +91,6 @@ public class Ball : MonoBehaviour
 
 		if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.Other )
         {
-			//print("hello 1");
 			if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {	
 				
@@ -160,7 +138,7 @@ public class Ball : MonoBehaviour
 			{ //allow the player to shoot five times then turn the ballforce off
 				Debug.Log ("Ap - Sp"+(Ap-Sp), gameObject);
 				BF = 12;
-				Debug.Log("BallForce OFF", gameObject);
+				//Debug.Log("BallForce OFF", gameObject);
 				temp.GetComponent<MeshRenderer> ().material = material2;
 				Sp = 0;
 				counter_switch = false;
@@ -178,7 +156,6 @@ public class Ball : MonoBehaviour
 	{
 			sm.ObstacleHit ();
 			im.GetComponent<Image> ().color = Color.red;
-			Debug.Log("You triggered", gameObject);
 			StartColorChange = ColorChange (0.5f);
 			StartCoroutine (StartColorChange);
 	}
@@ -188,7 +165,7 @@ public class Ball : MonoBehaviour
 
 	private IEnumerator ColorChange(float waitTime)
 	{
-		Debug.Log("Change color");
+		//Debug.Log("Change color");
 		yield return new WaitForSeconds(waitTime);
 		im.GetComponent<Image> ().color = Color.white;
 
