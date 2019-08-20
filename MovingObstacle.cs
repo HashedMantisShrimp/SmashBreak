63321fc7fb12afd303df25e296897592624c5969
PauloB04 <54282145+PauloB04@users.noreply.github.com>
2019-08-20 08:37:43 +0300

    Optimized Script

--- MovingObstacle.cs
+++ MovingObstacle.cs
@@ -1,40 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingObstacle : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start()
-	{
-
-
-
-	}
 	public float hspd = 3.0f;
-	// public float vspd = 15.0f;
 	public bool mov = true;
-	// public bool hit = false;
-	//  public double BkLim = -1.6;
-	//  public double FwdLim = -12.43;
 	public double USLim = -1.6;
 	public double DSLim = -12.43;
-	// public Rigidbody rb;
-
-
-
-	// Update is called once per frame
+	
 	void Update()
 	{
 		float trns = Time.deltaTime;
-		// rb = GetComponent<Rigidbody>();
-
-
+		
 		if (mov == true)
 		{
 
-			transform.Translate(1 * Vector3.forward * trns * hspd); //v3.up because when i set it to fwd it was moving upwards for whatever reason
+			transform.Translate(1 * Vector3.forward * trns * hspd); 
 		}
 		else
 		{
@@ -50,11 +33,5 @@ public class MovingObstacle : MonoBehaviour
 		{
 			mov = true;
 		}
-
-
-
-
 	}
-
-
 }
