447753d7a35684f48004ddb7bc45c99aa7b3ad02
PauloB04 <54282145+PauloB04@users.noreply.github.com>
2019-08-20 08:18:32 +0300

    Optimized AI script

--- AI.cs
+++ AI.cs
@@ -16,21 +16,19 @@ public class AI : MonoBehaviour {
 
 
 	void Update () {
-		//	Debug.Log ("inital speed "+gameObject.GetComponent<NavMeshAgent> ().speed); //3
-		//Debug.Log ("initial acc "+gameObject.GetComponent<NavMeshAgent> ().acceleration); //8
         gameObject.GetComponent<NavMeshAgent>().destination = goal.position;
 		if(gameObject.transform.position.z >= 46.14){
 			gameObject.GetComponent<NavMeshAgent> ().speed = 5.5f;
-			Debug.Log ("speed "+gameObject.GetComponent<NavMeshAgent> ().speed);
-			Debug.Log ("GotHere2");
+			//Debug.Log ("speed "+gameObject.GetComponent<NavMeshAgent> ().speed);
+			//Debug.Log ("GotHere2");
 
 		}
 
 		if (gameObject.transform.position.z >= 6.312417 && gameObject.transform.position.z <= 46.14) {
 			gameObject.GetComponent<NavMeshAgent> ().speed = 4.5f;
 			gameObject.GetComponent<NavMeshAgent> ().acceleration = 9.5f;
-			Debug.Log ("speed "+gameObject.GetComponent<NavMeshAgent> ().speed);
-			Debug.Log ("GotHere1");
+			//Debug.Log ("speed "+gameObject.GetComponent<NavMeshAgent> ().speed);
+			//Debug.Log ("GotHere1");
 		
 		}
 	
