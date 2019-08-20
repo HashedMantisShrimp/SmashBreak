a431c9a0a305609b75402d99277872219cb79e77
PauloB04 <54282145+PauloB04@users.noreply.github.com>
2019-08-20 08:16:07 +0300

    Optimized RobotScript

--- RobotScript.cs
+++ RobotScript.cs
@@ -1,24 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-//using TMPro;
 
 public class RobotScript : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
-       // projectile = GetComponent<Rigidbody>();
-		//BallC = GameObject.GetComponent<TextMeshProUGUI>();
-    }
     public float hspd = 50.0f;
     public float vspd = 15.0f;
 
-
-    // Update is called once per frame
     void Update()
     {
         float translation = Time.deltaTime;
@@ -28,32 +17,11 @@ public class RobotScript : MonoBehaviour
         if (Input.GetButtonDown("Cancel"))
         {
             Application.Quit(); 
-            print("cancelled");
-         //   SceneManager.LoadScene("Unity2ndCls 1", LoadSceneMode.Additive);
+            //print("cancelled");
             gameObject.SetActive(false);
         }
 
-           
-
     }
 
-
-    /*void OnCollisionEnter(Collision collision)
-    {
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            // Debug.DrawRay(contact.point, contact.normal, Color.red);
-             Debug.Log("You colided");
-
-        }
-
-    }
-
-
-    void OnTriggerEnter(Collider other)
-    {
-        Destroy(other.gameObject);
-        Debug.Log("You triggered", gameObject);
-    }*/
 }
 
